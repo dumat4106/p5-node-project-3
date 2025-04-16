374591d8448178600f96f842afaaa78b69c4b07d
Matthew Du <johndoe@example.com>
2025-04-16 03:27:46 -0500

    socket function added

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ var app = express();
 var server = app.listen(3000);
 
 app.use(express.static('public'));
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log(socket);
+
+    socket.on('mouse', mouseMsg)
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+}
